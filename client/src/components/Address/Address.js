31d587fabf86f1e6ce36bef9b400ec901183464e
peterhendrick <peterjhendrick@gmail.com>
2019-04-25 23:15:42 -0600

    commented out blockchain.info

--- client/src/components/Address/Address.js
+++ client/src/components/Address/Address.js
@@ -5,28 +5,28 @@ class Address extends React.Component {
     constructor(props) {
         super(props);
         this.state = { address: props.match.params.addrId };
-        this.testnetExplorer = blockexplorer.usingNetwork(3);
+        // this.testnetExplorer = blockexplorer.usingNetwork(3);
         this.componentDidMount = this.componentDidMount.bind(this);
     }
 
     async componentDidMount() {
         try {
             // const address = await blockexplorer.getAddress(this.state.address);
-            if(address) {
-                this.setState(state =>{
-                    state.addressObject = address;
-                });
-                console.log(this.state.addressObject);
-            }
+            // if(address) {
+            //     this.setState(state =>{
+            //         state.addressObject = address;
+            //     });
+            //     console.log(this.state.addressObject);
+            // }
         } catch (err) {
             console.log(err);
         }
 
-        const blockchainWS = new Socket({ network: 3 });
-        blockchainWS.onOpen(() => {
-            console.log('Websocket open');
-        });
-        blockchainWS.onTransaction(processTransaction, {addresses: [this.state.address.addr] });
+        // const blockchainWS = new Socket({ network: 3 });
+        // blockchainWS.onOpen(() => {
+        //     console.log('Websocket open');
+        // });
+        // blockchainWS.onTransaction(processTransaction, {addresses: [this.state.address.addr] });
         // const address = await this.testnetExplorer.getAddress(this.state.address.addr);
         // console.log(address);
 
