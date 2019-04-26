87d8be10fce665e946d04a4aa836454df0334378
peterhendrick <peterjhendrick@gmail.com>
2019-04-26 00:53:17 -0600

    responding with the address route

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import { blockexplorer } from 'blockchain.info';
+import { blockexplorer, exchange } from 'blockchain.info';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
@@ -26,8 +26,8 @@ app.route('/transaction/:txId').get(async (req: express.Request, res: express.Re
     const txId = req.params.txId;
     const testnetExplorer = blockexplorer.usingNetwork(3);
     try {
-        const tx = await testnetExplorer.getTx(txId);
-        if (!tx) res.render('index'); // TODO: 404
+        const tx = await Promise.all([testnetExplorer.getTx(txId), exchange.getTicker()]);
+        if (!tx) res.json({address: '404 not found'}); // TODO: 404
 
         res.json(tx);
     } catch (err) {
@@ -38,14 +38,16 @@ app.route('/transaction/:txId').get(async (req: express.Request, res: express.Re
 });
 
 app.route('/address/:addr')
-    .get(async (req: express.Request, res: express.Response) => {
+.get(async (req: express.Request, res: express.Response) => {
         const addr = req.params.addr;
         const testnetExplorer = blockexplorer.usingNetwork(3);
         try {
-            const address = await testnetExplorer.getAddress(addr);
+            const [address, ticker] = await Promise.all([testnetExplorer.getAddress(addr), exchange.getTicker()]);
+            // const address = await testnetExplorer.getAddress(addr);
             if (!address) res.render('index'); // TODO: 404
+            const response = { address, ticker };
 
-            res.json(address);
+            res.json(response);
         } catch (err) {
             console.log(err);
             res.json(err);
