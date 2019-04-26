8d0f50903b08ec3099530d771d5b420f071881d1
peterhendrick <peterjhendrick@gmail.com>
2019-04-26 01:06:43 -0600

    fetching transaction

--- client/src/components/Transaction/Transaction.js
+++ client/src/components/Transaction/Transaction.js
@@ -5,13 +5,27 @@ import './Transaction.css';
 class Transaction extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { transaction: { txid: props.match.params.transaction }  };
+        this.state = { transaction: { txId: props.match.params.txId }  };
         this.testnetExplorer = blockexplorer.usingNetwork(3);
         this.componentDidMount = this.componentDidMount.bind(this);
+        this.fetchTransaction = this.fetchTransaction.bind(this);
     }
 
-    async componentDidMount() {
+    componentDidMount() {
+        this.fetchTransaction()
+    }
 
+    fetchTransaction() {
+        fetch('/transaction/' + this.state.transaction.txId)
+            .then(response => response.json())
+            .then(data => {
+                this.setState(state => {
+                    state.transaction = data.tx;
+                    state.ticker = data.ticker;
+                    return state;
+                });
+                console.log(this.state);
+            })
     }
 
 
