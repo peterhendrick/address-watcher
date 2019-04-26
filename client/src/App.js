2689220e68ea0eb51473da12bbf14378ac2cf7cd
peterhendrick <peterjhendrick@gmail.com>
2019-04-25 23:15:24 -0600

    changed to hashed router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { Address } from './components/Address';
 import { Transaction } from './components/Transaction';
-import { history } from './history';
 
 
 class App extends React.Component {
@@ -14,13 +13,13 @@ class App extends React.Component {
     render() {
         return (
             <div >
-                <Router history={history}>
+                <HashRouter>
                     <div>
                         <Route exact path="/" component={Home} />
                         <Route path="/address/:addrId" component={Address} />
                         <Route path="/transaction/:txId" component={Transaction} />
                     </div>
-                </Router>
+                </HashRouter>
             </div>
         );
     }
