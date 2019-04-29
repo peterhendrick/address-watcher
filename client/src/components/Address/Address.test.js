72fac7a01af9eb9e4b696ff992143e31a530c092
peterhendrick <peterjhendrick@gmail.com>
2019-04-28 20:23:23 -0600

    working on tests

--- client/src/components/Address/Address.test.js
+++ client/src/components/Address/Address.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Address from './Address';
 
-it('renders without crashing', () => {
+it('gets a server response', async () => {
+    console.log('hi');
+    Address.fetchAddress();
 //   const div = document.createElement('div');
 //   ReactDOM.render(<Address />, div);
 //   ReactDOM.unmountComponentAtNode(div);
