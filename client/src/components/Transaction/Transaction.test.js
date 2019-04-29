c7bfcea6485afc3fd0d5d3a263d9a233cf48db66
peterhendrick <peterjhendrick@gmail.com>
2019-04-28 22:51:46 -0600

    commented out code

--- client/src/components/Transaction/Transaction.test.js
+++ client/src/components/Transaction/Transaction.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Transaction from './Transaction';
+import enzyme from 'enzyme'
+import * as Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter(Transaction) });
 
 it('renders without crashing', () => {
 //   const div = document.createElement('div');
-//   ReactDOM.render(<Transaction />, div);
+//   const wrap = enzyme.shallow(
+//       <Transaction />
+//   );
+//   wrap.setProps({ match: {params: { txId: "7f8467b7ae42fe2cc4befe63b8e705c539d0e919c9c510ef3602051056395a55" }}});
+//   ReactDOM.render(wrap, div);
 //   ReactDOM.unmountComponentAtNode(div);
 });
