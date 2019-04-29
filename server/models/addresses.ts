cd2fb0d7f5fa5b5d4713ff4d7e084ee5c013f730
peterhendrick <peterjhendrick@gmail.com>
2019-04-28 19:05:38 -0600

    made address required

--- server/models/addresses.ts
+++ server/models/addresses.ts
@@ -8,12 +8,10 @@ export interface IAddr extends mongoose.Document {
 }
 
 export const AddressSchema = new mongoose.Schema({
-    addr: String,
+    addr: {type: String, required: true},
     finalBalance: Number,
     txs: [Object]
 });
 
-// Export Mongoose "Book" model
 const Address = mongoose.model<IAddr>('Address', AddressSchema);
 export default Address;
-// module.exports = mongoose.model('Transaction', TxSchema);
